adc5265799bf3e3fb76f3094629bb25b068e328a
Giustino Borzacchiello <giustinob@gmail.com>
2015-02-05 11:03:05 +0100

    Added default route

--- src/FoodPanda/CountryBundle/Pastebin.php
+++ src/FoodPanda/CountryBundle/Pastebin.php
@@ -13,7 +13,7 @@ class Pastebin {
     public function getList()
     {
         $rawCountryList = file_get_contents( 'http://pastebin.com/raw.php?i=943PQQ0n' );
-        
+
         $rawCountryList = implode( "\n", array_slice( explode( "\n", $rawCountryList ), 3 ) );
         $rawCountryList = explode( "\r\n", $rawCountryList );
         array_pop( $rawCountryList );
